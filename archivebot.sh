d7406267657faf10eb0b19d9709b617c5915657b
MarcoAurelioWM <30000615+MarcoAurelioWM@users.noreply.github.com>
2017-11-24 23:25:29 +0100

    Create archivebot.sh

---
+++ archivebot.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+cd /data/project/mabot/scripts
+python archivebot.py User:MABot/config -family:wikibooks -lang:es
+python archivebot.py User:MABot/config -family:wikibooks -lang:eu
+python archivebot.py User:MABot/config -family:wikibooks -lang:gl
+python archivebot.py User:MABot/config -family:wikipedia -lang:an
+python archivebot.py User:MABot/config -family:wikipedia -lang:ang
+python archivebot.py User:MABot/config -family:wikipedia -lang:es
+python archivebot.py User:MABot/config -family:wikipedia -lang:eu
+python archivebot.py User:MABot/config -family:wikipedia -lang:ext
+python archivebot.py User:MABot/config -family:wikipedia -lang:jam
+python archivebot.py User:MABot/config -family:wikipedia -lang:olo
+python archivebot.py User:MABot/config -family:wikipedia -lang:tcy
+python archivebot.py User:MABot/config -family:wikiquote -lang:eu
+python archivebot.py User:MABot/config -family:wikiquote -lang:gl
+python archivebot.py User:MABot/config -family:wikisource -lang:es
+python archivebot.py User:MABot/config -family:wiktionary -lang:an
+python archivebot.py User:MABot/config -family:wiktionary -lang:ast
+python archivebot.py User:MABot/config -family:wiktionary -lang:gl
+python archivebot.py User:MABot/config -family:species -lang:species
