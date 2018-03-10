a0f5326a53254f7c869b81501ea624da5ada671c
MarcoAurelioWM <30000615+MarcoAurelioWM@users.noreply.github.com>
2018-03-10 14:16:19 +0100

    Update archivebot.sh

--- archivebot.sh
+++ archivebot.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 cd /data/project/mabot/scripts
+python archivebot.py User:MABot/config -family:species -lang:species
 python archivebot.py User:MABot/config -family:wikibooks -lang:es
 python archivebot.py User:MABot/config -family:wikibooks -lang:eu
 python archivebot.py User:MABot/config -family:wikibooks -lang:gl
+python archivebot.py User:MABot/config -family:wikimania -lang:2018
 python archivebot.py User:MABot/config -family:wikipedia -lang:an
 python archivebot.py User:MABot/config -family:wikipedia -lang:ang
 python archivebot.py User:MABot/config -family:wikipedia -lang:es
@@ -14,9 +16,7 @@ python archivebot.py User:MABot/config -family:wikipedia -lang:tcy
 python archivebot.py User:MABot/config -family:wikiquote -lang:eu
 python archivebot.py User:MABot/config -family:wikiquote -lang:gl
 python archivebot.py User:MABot/config -family:wikisource -lang:es
+python archivebot.py User:MABot/config -family:wikivoyage -lang:es
 python archivebot.py User:MABot/config -family:wiktionary -lang:an
 python archivebot.py User:MABot/config -family:wiktionary -lang:ast
 python archivebot.py User:MABot/config -family:wiktionary -lang:gl
-python archivebot.py User:MABot/config -family:species -lang:species
-python archivebot.py User:MABot/config -family:wikimania -lang:2018
-python archivebot.py User:MABot/config -family:wikivoyage -lang:es
