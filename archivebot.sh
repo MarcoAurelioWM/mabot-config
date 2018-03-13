7cdfa76e41c2a4be86f85f5859242fb75b0dc5ed
MarcoAurelioWM <30000615+MarcoAurelioWM@users.noreply.github.com>
2018-03-13 13:26:48 +0100

    archivebot: add es.wikiquote

--- archivebot.sh
+++ archivebot.sh
@@ -12,6 +12,7 @@ python archivebot.py User:MABot/config -family:wikipedia -lang:jam
 python archivebot.py User:MABot/config -family:wikipedia -lang:olo
 python archivebot.py User:MABot/config -family:wikipedia -lang:tcy
 python archivebot.py User:MABot/config -family:wikiquote -lang:eu
+python archivebot.py User:MABot/config -family:wikiquote -lang:es
 python archivebot.py User:MABot/config -family:wikiquote -lang:gl
 python archivebot.py User:MABot/config -family:wikisource -lang:es
 python archivebot.py User:MABot/config -family:wiktionary -lang:an
