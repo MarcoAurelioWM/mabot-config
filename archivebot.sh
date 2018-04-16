f456eaa41440da57f22aedc1e31f6d8cc3c60135
MarcoAurelioWM <30000615+MarcoAurelioWM@users.noreply.github.com>
2018-04-16 12:52:19 +0200

    archivebot: add eswikinews
    locally approved for this task

--- archivebot.sh
+++ archivebot.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 cd /data/project/mabot/scripts
+python archivebot.py User:MABot/config -family:species -lang:species
 python archivebot.py User:MABot/config -family:wikibooks -lang:es
 python archivebot.py User:MABot/config -family:wikibooks -lang:eu
 python archivebot.py User:MABot/config -family:wikibooks -lang:gl
+python archivebot.py User:MABot/config -family:wikimania -lang:2018
+python archivebot.py User:MABot/config -family:wikinews -lang:es
 python archivebot.py User:MABot/config -family:wikipedia -lang:an
 python archivebot.py User:MABot/config -family:wikipedia -lang:ang
 python archivebot.py User:MABot/config -family:wikipedia -lang:es
@@ -11,14 +14,12 @@ python archivebot.py User:MABot/config -family:wikipedia -lang:ext
 python archivebot.py User:MABot/config -family:wikipedia -lang:jam
 python archivebot.py User:MABot/config -family:wikipedia -lang:olo
 python archivebot.py User:MABot/config -family:wikipedia -lang:tcy
-python archivebot.py User:MABot/config -family:wikiquote -lang:eu
 python archivebot.py User:MABot/config -family:wikiquote -lang:es
+python archivebot.py User:MABot/config -family:wikiquote -lang:eu
 python archivebot.py User:MABot/config -family:wikiquote -lang:gl
 python archivebot.py User:MABot/config -family:wikisource -lang:es
+python archivebot.py User:MABot/config -family:wikivoyage -lang:es
 python archivebot.py User:MABot/config -family:wiktionary -lang:an
 python archivebot.py User:MABot/config -family:wiktionary -lang:ast
 python archivebot.py User:MABot/config -family:wiktionary -lang:es
 python archivebot.py User:MABot/config -family:wiktionary -lang:gl
-python archivebot.py User:MABot/config -family:species -lang:species
-python archivebot.py User:MABot/config -family:wikimania -lang:2018
-python archivebot.py User:MABot/config -family:wikivoyage -lang:es
