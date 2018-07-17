d6da1ec4bc4aa513278ae6f4f10a72b09f4728df
MarcoAurelioWM <30000615+MarcoAurelioWM@users.noreply.github.com>
2018-07-17 12:34:06 +0200

    Update clean-sandbox.sh

--- clean-sandbox.sh
+++ clean-sandbox.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /data/project/mabot/scripts
-python clean_sandbox.py -pageid:1805,1806,1807,1808,1809,1814,1813,1812,1811,1810 -family:wikivoyage -lang:es -summary:"Bot en pruebas: limpiando la zona de pruebas"
+python clean_sandbox.py -pageid:1805,1806,1807,1808,1809,1814,1813,1812,1811,1810 -family:wikivoyage -lang:es -text:"{{subst:User:MABot/zdp}}" -delay:15 -summary:"Bot en pruebas: limpiando la zona de pruebas"
