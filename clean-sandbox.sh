461f217462640793935a9b1060d9e1f639dfb72d
MarcoAurelio <30000615+MarcoAurelioWM@users.noreply.github.com>
2019-02-06 22:43:00 +0100

    clean-sandbox: Add support for meta.wikimedia

--- clean-sandbox.sh
+++ clean-sandbox.sh
@@ -3,3 +3,4 @@ cd /data/project/mabot/scripts
 python clean_sandbox.py -pageid:1805,1806,1807,1808,1809,1814,1813,1812,1811,1810 -family:wikivoyage -lang:es -text:"{{subst:User:MABot/zdp}}" -delay:15 -summary:"Bot: limpieza automática de la zona de pruebas"
 python clean_sandbox.py -pageid:47 -family:wikiquote -lang:es -text:"{{subst:User:MABot/zdp}}" -delay:15 -summary:"Bot: limpieza automática de la zona de pruebas"
 python clean_sandbox.py -pageid:129470,166832,129475,129476,129477 -family:wikisource -lang:es -text:"<!--No borres este mensaje-->{{Zona de pruebas}}<!--Haz las pruebas debajo. Gracias-->" -delay:15 -summary:"Bot: limpieza automática de la zona de pruebas"
+python clean_sandbox.py -pageid:10739043 -text:"{{Meta:Sandbox/Please do not edit this line}}<!-- Please edit below this line. -->" -family:meta -lang:meta -delay:15 -summary:"Bot: clearing the sandbox"
