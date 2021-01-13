dc6e950911196a92bb3325caf0e9d2c5ed96f065
MarcoAurelio <30000615+MarcoAurelioWM@users.noreply.github.com>
2021-01-13 14:15:04 +0100

    Update archivebot.sh

--- archivebot.sh
+++ archivebot.sh
@@ -13,7 +13,6 @@ python3 archivebot.py User:MABot/config -family:wikipedia -lang:ext
 python3 archivebot.py User:MABot/config -family:wikipedia -lang:jam
 python3 archivebot.py User:MABot/config -family:wikipedia -lang:olo
 python3 archivebot.py User:MABot/config -family:wikipedia -lang:sco
-python3 archivebot.py User:MABot/config -family:wikipedia -lang:tcy
 python3 archivebot.py User:MABot/config -family:wikiquote -lang:en
 python3 archivebot.py User:MABot/config -family:wikiquote -lang:es
 python3 archivebot.py User:MABot/config -family:wikiquote -lang:eu
