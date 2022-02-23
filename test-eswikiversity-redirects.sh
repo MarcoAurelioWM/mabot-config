626d26446ff345a92eb3f70a08b18621f01e0ae5
MarcoAurelio <30000615+MarcoAurelioWM@users.noreply.github.com>
2022-02-23 18:42:32 +0100

    Delete test-eswikiversity-redirects.sh

--- test-eswikiversity-redirects.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Spanish Wikiversity (in trial; 1 edit each 2 minutes)
-python redirect.py do -namespace:not:2,3 -family:wikiversity -lang:es -always -pt:120
-python redirect.py br -namespace:not:2,3 -family:wikiversity -sdtemplate:"{{Destruir|1=Bot: redirección rota}}" -lang:es -delete -always -pt:120
