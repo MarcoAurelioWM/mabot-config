26ffd70d15e9a919d4e00ffead841365bb67110f
MarcoAurelio <30000615+MarcoAurelioWM@users.noreply.github.com>
2022-02-23 17:46:57 +0100

    Update test-eswikiversity-redirects.sh

--- test-eswikiversity-redirects.sh
+++ test-eswikiversity-redirects.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Spanish Wikiversity (in trial; 1 edit each 2 minutes)
-python3 redirect.py do -namespace:not:2,3 -family:wikiversity -lang:es -always -pt:120
-python3 redirect.py br -namespace:not:2,3 -family:wikiversity -sdtemplate:"{{Destruir|1=Bot: redirección rota}}" -lang:es -delete -always -pt:120
+python redirect.py do -namespace:not:2,3 -family:wikiversity -lang:es -always -pt:120
+python redirect.py br -namespace:not:2,3 -family:wikiversity -sdtemplate:"{{Destruir|1=Bot: redirección rota}}" -lang:es -delete -always -pt:120
