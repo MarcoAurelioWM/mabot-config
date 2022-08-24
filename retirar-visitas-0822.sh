e8a68076ba7094110ab2af7aa61c053d2e973a06
MarcoAurelio <30000615+MarcoAurelioWM@users.noreply.github.com>
2022-08-24 17:10:21 +0200

    Remove retirar-visitas task

--- retirar-visitas-0822.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /data/project/mabot/scripts
-python3 template.py Visitas -family:wikipedia -lang:es -remove -always -summary:"Bot: retirando plantilla según lo resuelto en la [[Wikipedia:Consultas de borrado/Plantilla:Visitas|consulta de borrado]]"
