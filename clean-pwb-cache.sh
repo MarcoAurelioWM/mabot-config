a495f3c29cf0e8dd2d825e23f3661dc95442f00e
MarcoAurelio <30000615+MarcoAurelioWM@users.noreply.github.com>
2023-01-25 00:13:05 +0100

    Update clean-pwb-cache.sh

--- clean-pwb-cache.sh
+++ clean-pwb-cache.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pwb="$HOME/pwbvenv/bin/pwb"
-echo -e "Starting deletion of Pywikibot API cache data located at '$HOME/apicache.py' on $(date)"
-$pwb cache -delete $HOME/apicache-py3
+echo -e "Starting deletion of Pywikibot API cache data located at '$HOME/apicache-py3' on $(date)"
+$pwb cache -delete -v $HOME/apicache-py3
 echo -e "Deletion complete: $(date)"
 printf '%20s\n' | tr ' ' -
