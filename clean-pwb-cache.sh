1f3b93928cced6fc35ff4bcade858f3e8feecba7
MarcoAurelio <30000615+MarcoAurelioWM@users.noreply.github.com>
2023-01-24 23:46:17 +0100

    Trying a sytax update for clean-pwb-cache

--- clean-pwb-cache.sh
+++ clean-pwb-cache.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-$HOME/pwbvenv/bin/python3 $HOME/pywikibot-core/pwb.py cache -delete $HOME/apicache-py3
+py3="$HOME/pwbvenv/bin/python3"
+pwb="$HOME/pywikibot-core/pwb.py"
+echo -e "Starting deletion of Pywikibot API cache data located at $HOME/apicache.py on $(date)"
+$py3 $pwb cache -delete $HOME/apicache-py3
+echo -e "Deletion complete: $(date)"
+printf '%20s\n' | tr ' ' -
