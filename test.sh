864ea1b540224145a5be1642035c7f609d78ede6
MarcoAurelio <30000615+MarcoAurelioWM@users.noreply.github.com>
2023-01-25 13:36:39 +0100

    Delete test.sh

--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-array=("$HOME/pwbvenv/bin/python3" "$HOME/pywikibot-core/pwb.py" "archivebot User:MABot/config")
-echo -e "Starting archiver bot module at $(date)"
-printf '%40s\n' | tr ' ' -
-${array[*]} -family:species -lang:species
