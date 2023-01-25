acb04b16a32347a367d6761369f21f3daa38418e
MarcoAurelio <30000615+MarcoAurelioWM@users.noreply.github.com>
2023-01-25 22:09:21 +0100

    Update archivebot.sh

--- archivebot.sh
+++ archivebot.sh
@@ -1,37 +1,35 @@
 #!/bin/bash
 
-py3="$HOME/pwbvenv/bin/python3"
-pwb="$HOME/pywikibot-core/pwb.py"
-static="archivebot User:MABot/config"
+array=("$HOME/pwbvenv/bin/python3" "$HOME/pywikibot-core/pwb.py" "archivebot" "User:MABot/config")
 echo -e "Starting archiver bot module at $(date)"
-printf '%20s\n' | tr ' ' +
-$py3 $pwb $static -family:species -lang:species
 printf '%20s\n' | tr ' ' -
-$py3 $pwb $static -family:wikibooks -lang:es
-$py3 $pwb $static -family:wikibooks -lang:eu
-$py3 $pwb $static -family:wikibooks -lang:gl
+${array[*]} -family:species -lang:species
 printf '%20s\n' | tr ' ' -
-$py3 $pwb $static -family:wikinews -lang:es
+${array[*]} -family:wikibooks -lang:es
+${array[*]} -family:wikibooks -lang:eu
+${array[*]} -family:wikibooks -lang:gl
 printf '%20s\n' | tr ' ' -
-$py3 $pwb $static -family:wikipedia -lang:an
-$py3 $pwb $static -family:wikipedia -lang:ang
-$py3 $pwb $static -family:wikipedia -lang:es
-$py3 $pwb $static -family:wikipedia -lang:eu
-$py3 $pwb $static -family:wikipedia -lang:sco
-$py3 $pwb $static -family:wikipedia -lang:tcy
+${array[*]} -family:wikinews -lang:es
 printf '%20s\n' | tr ' ' -
-$py3 $pwb $static -family:wikiquote -lang:en
-$py3 $pwb $static -family:wikiquote -lang:es
-$py3 $pwb $static -family:wikiquote -lang:eu
-$py3 $pwb $static -family:wikiquote -lang:gl
+${array[*]} -family:wikipedia -lang:an
+${array[*]} -family:wikipedia -lang:ang
+${array[*]} -family:wikipedia -lang:es
+${array[*]} -family:wikipedia -lang:eu
+${array[*]} -family:wikipedia -lang:sco
+${array[*]} -family:wikipedia -lang:tcy
 printf '%20s\n' | tr ' ' -
-$py3 $pwb $static -family:wikisource -lang:es
+${array[*]} -family:wikiquote -lang:en
+${array[*]} -family:wikiquote -lang:es
+${array[*]} -family:wikiquote -lang:eu
+${array[*]} -family:wikiquote -lang:gl
 printf '%20s\n' | tr ' ' -
-$py3 $pwb $static -family:wikivoyage -lang:es
+${array[*]} -family:wikisource -lang:es
 printf '%20s\n' | tr ' ' -
-$py3 $pwb $static -family:wiktionary -lang:an
-$py3 $pwb $static -family:wiktionary -lang:ast
-$py3 $pwb $static -family:wiktionary -lang:es
-$py3 $pwb $static -family:wiktionary -lang:gl
+${array[*]} -family:wikivoyage -lang:es
+printf '%20s\n' | tr ' ' -
+${array[*]} -family:wiktionary -lang:an
+${array[*]} -family:wiktionary -lang:ast
+${array[*]} -family:wiktionary -lang:es
+${array[*]} -family:wiktionary -lang:gl
 echo -e "Archiver bot module finished at $(date)"
-printf '%20s\n' | tr ' ' +
+printf '%20s\n' | tr ' ' -
