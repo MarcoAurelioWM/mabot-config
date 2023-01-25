585f5a0a76599691ca57bd932dd51af3db457541
MarcoAurelio <30000615+MarcoAurelioWM@users.noreply.github.com>
2023-01-25 13:42:39 +0100

    Update archivebot-wikitech.sh

--- archivebot-wikitech.sh
+++ archivebot-wikitech.sh
@@ -2,7 +2,7 @@
 
 array=("$HOME/pwbvenv/bin/python3" "$HOME/pywikibot-core/pwb.py" "archivebot User:MABot/config")
 echo -e "Running archivebot module for wikitech.wikimedia.org on $(date)."
-printf '%40s\n' | tr ' ' -
+printf '%60s\n' | tr ' ' -
 ${array[*]} -family:wikitech -lang:en
 echo -e "Script finished at $(date)."
-printf '%20s\n' | tr ' ' -
+printf '%60s\n' | tr ' ' -
