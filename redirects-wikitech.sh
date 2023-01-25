656285b6917fcc404911fde1a2f2b9a06dbb0054
MarcoAurelio <30000615+MarcoAurelioWM@users.noreply.github.com>
2023-01-25 19:58:36 +0100

    Update redirects-wikitech.sh

--- redirects-wikitech.sh
+++ redirects-wikitech.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 array=("$HOME/pwbvenv/bin/python3" "$HOME/pywikibot-core/pwb.py" "redirect")
+printf '%60s\n' | tr ' ' +
 echo -e "Maintenance of double and broken redirects for Wikitech started at $(date)."
-printf '%20s\n' | tr ' ' +
+printf '%60s\n' | tr ' ' +
 echo -e "Taking care of DoubleRedirects"
 printf '%20s\n' | tr ' ' -
 ${array[*]} do -family:wikitech -lang:en -always
