f34db3d3b5ce19dd5861e59fa61ae458a2b1bce5
MarcoAurelio <30000615+MarcoAurelioWM@users.noreply.github.com>
2023-01-25 17:23:47 +0100

    Update catredir-eswiki.sh

--- catredir-eswiki.sh
+++ catredir-eswiki.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+echo -e "Starting category redirect script for eswiki at $(date)."
+printf '%20s\n' | tr ' ' -
 $HOME/pwbvenv/bin/python3 $HOME/pywikibot-core/pwb.py category_redirect -family:wikipedia -lang:es
