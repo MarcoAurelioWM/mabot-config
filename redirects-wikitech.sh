1f4d137e7ac7a5b407c5c8676b29d4c724659877
MarcoAurelio <30000615+MarcoAurelioWM@users.noreply.github.com>
2023-01-25 19:48:44 +0100

    Update redirects-wikitech.sh

--- redirects-wikitech.sh
+++ redirects-wikitech.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-$HOME/pwbvenv/bin/python3 $HOME/pywikibot-core/pwb.py redirect do -family:wikitech -lang:en -always
-$HOME/pwbvenv/bin/python3 $HOME/pywikibot-core/pwb.py redirect br -family:wikitech -lang:en -delete -always -sdtemplate:"{{delete|reason=Bot: broken redirect}}"
+array=("$HOME/pwbvenv/bin/python3" "$HOME/pywikibot-core/pwb.py" "redirect")
+echo -e "Maintenance of double and broken redirects for Wikitech started at $(date)."
+printf '%20s\n' | tr ' ' +
+echo -e "Taking care of DoubleRedirects"
+printf '%20s\n' | tr ' ' -
+${array[*]} do -family:wikitech -lang:en -always
+echo -e "Taking care of BrokenRedirects"
+printf '%20s\n' | tr ' ' -
+${array[*]} br -family:wikitech -lang:en -delete -always -sdtemplate:"{{delete|reason=Bot: broken redirect}}"
+echo -e "Script finished at $(date)."
