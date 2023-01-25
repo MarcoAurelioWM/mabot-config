e4f5c0dcdfcdd3fd9275e0859100669b6f1fd11e
MarcoAurelio <30000615+MarcoAurelioWM@users.noreply.github.com>
2023-01-25 12:36:18 +0100

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -v
+arr=($HOME/pwbvenv/bin/python3 $HOME/pywikibot-core/pwb.py archivebot User:MABot/config)
+echo -e "Starting archiver bot module at $(date)"
+printf '%20s\n' | tr ' ' -
+$arr -family:species -lang:en
