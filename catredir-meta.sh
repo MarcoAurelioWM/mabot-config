2c63774d4b2466983925a4d77b85aeb62ae15089
MarcoAurelio <30000615+MarcoAurelioWM@users.noreply.github.com>
2023-03-04 14:13:49 +0100

    Update catredir-meta.sh

--- catredir-meta.sh
+++ catredir-meta.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+
+echo -e "\nStarting category redirect script for metawiki at $(date)."
+printf '%60s\n' | tr ' ' -
 $HOME/pwbvenv/bin/python3 $HOME/pywikibot-core/pwb.py category_redirect -family:meta -lang:meta
+echo -e "Script finished at $(date)."
