61112a03765d6a5e657fd32cd6fcb045932f0927
MA <30000615+MarcoAurelioWM@users.noreply.github.com>
2025-02-17 17:38:32 +0100

    catredir-meta: Pass `-always` argument
    Apparently needed now for the bot to work.

--- catredir-meta.sh
+++ catredir-meta.sh
@@ -2,5 +2,5 @@
 
 echo -e "\nStarting category redirect script for metawiki at $(date)."
 printf '%60s\n' | tr ' ' -
-$HOME/pwbvenv/bin/python3 $HOME/pywikibot-core/pwb.py category_redirect -site:meta
+$HOME/pwbvenv/bin/python3 $HOME/pywikibot-core/pwb.py category_redirect -site:meta -always
 echo -e "Script finished at $(date)."
