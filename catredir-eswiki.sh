bad2db36deaf8216268ddcbb93e50d650a97c7ea
MA <30000615+MarcoAurelioWM@users.noreply.github.com>
2025-02-17 17:54:27 +0100

    Update catredir-eswiki.sh

--- catredir-eswiki.sh
+++ catredir-eswiki.sh
@@ -2,5 +2,5 @@
 
 echo -e "Starting category redirect script for eswiki at $(date)."
 printf '%60s\n' | tr ' ' -
-$HOME/pwbvenv/bin/python3 $HOME/pywikibot-core/pwb.py category_redirect -family:wikipedia -lang:es
+$HOME/pwbvenv/bin/python3 $HOME/pywikibot-core/pwb.py category_redirect -site:wikipedia:es -always
 echo -e "\nCategory redirect maintenance script for eswiki finished at $(date)."
