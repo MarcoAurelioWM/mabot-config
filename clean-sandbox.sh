89cbd5cfe25dc0fdb1bb7692872d2dd9f6837375
MA <30000615+MarcoAurelioWM@users.noreply.github.com>
2026-06-07 19:21:12 +0200

    Remove Spanish Wikinews commands

--- clean-sandbox.sh
+++ clean-sandbox.sh
@@ -10,7 +10,6 @@ echo -e "Sandbox clearing script started at $(date)."
 printf '%50s\n' | tr ' ' -
 ${command[*]} -pageid:10957461 -text:"{{Meta:Sandbox/Please do not edit this line}}<!-- Please edit below this line -->" -family:meta -lang:meta -delay:15 -summary:"Bot: clearing the sandbox"
 ${command[*]} -pageid:37341 -text:"{{sust:ZDP/2}}" -family:wikibooks -lang:es -delay:15 -summary:"Bot: limpieza automática de la zona de pruebas"
-${command[*]} -pageid:13709 -text:"{{ZDP}}" -family:wikinews -lang:es -delay:15 -summary:"Bot: limpieza automática de la zona de pruebas"
 ${command[*]} -pageid:40294 -text:"{{ZDP}}" -family:wikiquote -lang:es -delay:15 -summary:"Bot: limpieza automática de la zona de pruebas"
 ${command[*]} -pageid:129470,166832,129475,129476,129477 -text:"<!--No borres este mensaje-->{{Zona de pruebas}}<!--Haz las pruebas debajo. Gracias-->" -family:wikisource -lang:es -delay:15 -summary:"Bot: limpieza automática de la zona de pruebas"
 ${command[*]} -pageid:5856 -text:"{{/encabezado}}<!-- REALIZA TUS PRUEBAS DEBAJO DE ESTA LÍNEA -->" -family:wikiversity -lang:es -delay:15 -summary:"Bot: limpieza automática de la zona de pruebas"
